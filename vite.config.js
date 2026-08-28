69a38fdabf0bf641d7d30d2ca380847e544297e7
buhroointeriors-dev <buhroointeriors@gmail.com>
2026-08-29 02:38:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// base must match your repository name exactly with slashes on both sides
 export default defineConfig({
   plugins: [react()],
-  base: '/buhroo-interiors/',
+  base: './', // Using relative path fixes blank screen asset loading on GitHub Pages
 })
