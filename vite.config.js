1fa6231bbe3472385426858a37de7c89943cb837
buhroointeriors-dev <buhroointeriors@gmail.com>
2026-08-29 11:15:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Using relative path fixes blank screen asset loading on GitHub Pages
-})
+  base: '/', // Use root path for custom domains
+});
